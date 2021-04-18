3c2f90a5d8ca9ea950dbf322165812a9f7192438
Pankaj <engg.pankaj98@gmail.com>
2021-04-18 22:27:20 +0530

    add

--- idpass.js
+++ idpass.js
@@ -1,8 +1,7 @@
 let phone = "9410557422";
 let pass = "pankajrocks";
-let filesel =".oajrlxb2.bp9cbjyn.g5ia77u1.mtkw9kbi.tlpljxtp.qensuy8j.ppp5ayq2.goun2846.ccm00jje.s44p3ltw.mk2mc5f4.rt8b4zig.n8ej3o3l.agehan2d.sk4xxmp2.rq0escxv.nhd2j8a9.j83agx80.rj1gh0hx.btwxx1t3.g5gj957u.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.tgvbjcpo.idt9hxom.cxgpxx05.dflh9lhu.sj5x9vvc.scb9dxdr.l9j0dhe7.i1ao9s8h.esuyzwwr.f1sip0of.du4w35lb.lzcic4wl.abiwlrkh.p8dawk7l.ue3kfks5.pw54ja7n.uo3d90p7.l82x9zwi.buofh1pr.taijpn5t"
+
 module.exports={
     phone:phone,
-    pass:pass,
-    filesel:filesel
+    pass:pass
 }
